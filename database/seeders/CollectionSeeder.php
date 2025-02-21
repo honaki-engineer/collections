4fd023e442bca8fc6c806be67069d0d097fbcef8
HondaAkihito <honaki.and.1114@gmail.com>
2025-02-21 21:31:20 +0900

    users collections ダミーデータ(seeder)

---
+++ database/seeders/CollectionSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CollectionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('collections')->insert([
+            [
+                'title' => 'タイトル',
+                'description' => 'アプリ解説',
+                'url_qiita' => 'https://qiita.com/',
+                'url_webapp' => null,
+                'url_github' => 'https://github.com/',
+                'is_public' => 0,
+                'position' => 0,
+                'user_id' => 1,
+            ],
+            [                
+                'title' => 'タイトル2',
+                'description' => 'アプリ解説2',
+                'url_qiita' => 'https://qiita.com/',
+                'url_webapp' => null,
+                'url_github' => 'https://github.com/',
+                'is_public' => 1,
+                'position' => 1,
+                'user_id' => 1,
+            ]
+        ]);
+    }
+}
