8c524e0925f142a44f84d480918856b03c063611
HondaAkihito <honaki.and.1114@gmail.com>
2025-03-28 09:14:27 +0900

    admin.technologyTags.edit 途中_データ表示まで完了

--- resources/views/admin/technologyTags/edit.blade.php
+++ resources/views/admin/technologyTags/edit.blade.php
@@ -20,21 +20,21 @@
                         {{-- 技術タグの種類 --}}
                         <div class="p-2 w-full" id="tech_type_section">
                           <div class="relative">
-                            <label class="leading-7 text-sm text-gray-600">▪️ 技術タグの種類</label>
-                            <input type="radio" name="tech_type" value="0">言語
-                            <input type="radio" name="tech_type" value="1">フレームワーク
-                            <input type="radio" name="tech_type" value="2">ツール
+                            <label class="leading-7 text-sm text-gray-600">技術タグの種類</label>
+                            <input type="radio" name="tech_type" value="0" @if($technologyTag->tech_type === 0) checked @endif>言語
+                            <input type="radio" name="tech_type" value="1" @if($technologyTag->tech_type === 1) checked @endif>フレームワーク
+                            <input type="radio" name="tech_type" value="2" @if($technologyTag->tech_type === 2) checked @endif>ツール
                           </div>
                         </div>
                         {{-- 名前 --}}
                         <div class="p-2 w-full">
                           <div class="relative">
-                            <label for="names" class="leading-7 text-sm text-gray-600">▪️ タグ<br>カンマ + 半角スペース区切りで、複数入力OK</label>
-                            <input type="text" id="name" name="names" placeholder="例)PHP, HTML, CSS" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                            <label for="names" class="leading-7 text-sm text-gray-600">タグ</label>
+                            <input type="text" id="name" name="names" value="{{ $technologyTag->name }}" placeholder="例)PHP, HTML, CSS" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                           </div>
                         </div>
                         <div class="p-2 w-full">
-                          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">新規登録する</button>
+                          <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                         </div>
                         </div>
                       </div>
