4bf6892ac95c9485829754bea0aa94b9e7064c99
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-04 16:23:39 +0900

    feature_tags migration user_id 追記

--- database/migrations/2025_04_04_161313_create_feature_tags_table.php
+++ database/migrations/2025_04_04_161313_create_feature_tags_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('feature_tags', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->timestamps();
         });
