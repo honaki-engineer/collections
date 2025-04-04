21e63bf3917d7c7a7ab14d97880c098e0ce7da51
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-04 16:34:52 +0900

    collection_feature migration

--- app/Models/FeatureTag.php
+++ app/Models/FeatureTag.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class FeatureTag extends Model
 {
     use HasFactory;
+
+    // protected $fillable = [
+    //     'name', 'user_id'
+    // ];
+
+    // リレーション
+    // public function collections() : belongsToMany
+    // {
+    //     return $this->belongsToMany(Collection::class, 'collection_technology');
+    // }
+    // public function user() : BelongsTo
+    // {
+    //     return $this->belongsTo(User::class);
+    // }
 }
