22b5bdb1e446ea746988b61190cd4fede3445131
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-09 15:56:00 +0900

    /public_site/show.blade.php Qiita画像 追加

--- resources/views/public_site/show.blade.php
+++ resources/views/public_site/show.blade.php
@@ -6,23 +6,37 @@
       <div class="grid md:grid-cols-3 gap-10">
         <!-- 左カラム：説明文 -->
         <div class="md:col-span-1">
-          <h2 class="text-2xl font-bold text-center mb-4">The Sample App</h2>
-          <p class="text-gray-700 mb-4">
-            Ruby on Railsのチュートリアルに沿って制作したWebアプリケーションです。Twitterのように小さなコメントを投稿したり、ユーザーをフォローして簡単なコミュニケーションが取れます。
-            このチュートリアルを通してテスト駆動開発、CRUD、ユーザー登録などの基礎を学びました。
-          </p>
+          <h2 class="text-2xl font-bold text-center mb-4">{{ $collection->title }}</h2>
+          @if($collection->description)
+            <p class="text-gray-700 mb-4">{!! nl2br(e($collection->description)) !!}</p>
+          @endif
 
           <div class="space-y-2">
-            <a href="https://rails-sample-app-by-hodaka.herokuapp.com/" target="_blank" class="inline-flex items-center text-blue-600 hover:underline">
-              <img src="{{ asset('asset/img/video-icon.svg') }}" alt="Demo" class="w-5 h-5 mr-2"> Demo
+            @if($collection->url_qiita)
+            <a href="{{ $collection->url_qiita }}" target="_blank" class="inline-flex items-center text-blue-600 hover:underline">
+              <img src="{{ asset('storage/collection_images/qiita.png') }}" alt="Demo" class="w-5 h-5 mr-2"> Qiita
             </a>
             <br>
-            <a href="https://github.com/hodanov/ruby-on-rails-tutorial" target="_blank" class="inline-flex items-center text-gray-800 hover:underline">
-              <img src="{{ asset('asset/img/github-icon.svg') }}" alt="GitHub" class="w-5 h-5 mr-2"> GitHub
+            @endif
+            @if($collection->url_webapp)
+            <a href="{{ $collection->url_webapp }}" target="_blank" class="inline-flex items-center text-gray-800 hover:underline">
+              <img src="{{ asset('asset/img/github-icon.svg') }}" alt="GitHub" class="w-5 h-5 mr-2"> 実際のページ
             </a>
+            <br>
+            @endif
+            @if($collection->url_github)
+            <a href="{{ $collection->url_github }}" target="_blank" class="inline-flex items-center text-gray-800 hover:underline">
+              <img src="{{ asset('asset/img/github-icon.svg') }}" alt="GitHub" class="w-5 h-5 mr-2"> Github
+            </a>
+            @endif
           </div>
 
           <h3 class="text-lg font-semibold mt-6 text-center">使用技術</h3>
+          <p class="text-sm text-gray-600">
+            Ruby on Rails, HTML/CSS(SASS), Bulma, jQuery, Sqlite（開発）,
+            Postgresql（本番）, Heroku, SendGrid
+          </p>
+          <h3 class="text-lg font-semibold mt-6 text-center">実装機能</h3>
           <p class="text-sm text-gray-600">
             Ruby on Rails, HTML/CSS(SASS), Bulma, jQuery, Sqlite（開発）,
             Postgresql（本番）, Heroku, SendGrid
