602f8ab5434c52a76f5e204c1cbdce956c06c6f6
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-09 16:01:10 +0900

    /public_site/show.blade.php github画像 追加

--- resources/views/public_site/show.blade.php
+++ resources/views/public_site/show.blade.php
@@ -26,7 +26,7 @@
             @endif
             @if($collection->url_github)
             <a href="{{ $collection->url_github }}" target="_blank" class="inline-flex items-center text-gray-800 hover:underline">
-              <img src="{{ asset('asset/img/github-icon.svg') }}" alt="GitHub" class="w-5 h-5 mr-2"> Github
+              <img src="{{ asset('storage/collection_images/github.png') }}" alt="GitHub" class="w-5 h-5 mr-2"> Github
             </a>
             @endif
           </div>
