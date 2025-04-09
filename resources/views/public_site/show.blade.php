8147846855871d22895a63c35a590155a590ebed
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-09 19:06:34 +0900

    /public_site/show.blade.php demo画像 追加

--- resources/views/public_site/show.blade.php
+++ resources/views/public_site/show.blade.php
@@ -18,15 +18,15 @@
             </a>
             <br>
             @endif
-            @if($collection->url_webapp)
-            <a href="{{ $collection->url_webapp }}" target="_blank" class="inline-flex items-center text-gray-800 hover:underline">
-              <img src="{{ asset('asset/img/github-icon.svg') }}" alt="GitHub" class="w-5 h-5 mr-2"> 実際のページ
-            </a>
-            <br>
-            @endif
             @if($collection->url_github)
             <a href="{{ $collection->url_github }}" target="_blank" class="inline-flex items-center text-gray-800 hover:underline">
               <img src="{{ asset('storage/collection_images/github.png') }}" alt="GitHub" class="w-5 h-5 mr-2"> Github
+            </a>
+            <br>
+            @endif
+            @if($collection->url_webapp)
+            <a href="{{ $collection->url_webapp }}" target="_blank" class="inline-flex items-center text-gray-800 hover:underline">
+              <img src="{{ asset('storage/collection_images/webApp.png') }}" alt="GitHub" class="w-5 h-5 mr-2"> Demo
             </a>
             @endif
           </div>
