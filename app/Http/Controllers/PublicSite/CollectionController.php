131a5881eeb9ad8cd978a1511d98c5bb88cccaec
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-10 10:58:29 +0900

    CollectionService.php 関数 追加

--- app/Http/Controllers/PublicSite/CollectionController.php
+++ app/Http/Controllers/PublicSite/CollectionController.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers\PublicSite;
 use App\Http\Controllers\Controller;
 use App\Models\Collection;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use App\Service\PublicSite\CollectionService;
+
 
 class CollectionController extends Controller
 {
@@ -62,24 +62,8 @@ class CollectionController extends Controller
      */
     public function show($id)
     {
-        $collection = Collection::with([
-            'collectionImages' => fn($query) => $query->orderBy('position', 'asc'),
-            'technologyTags' => fn($query) => $query->orderBy('tech_type', 'asc'),
-            'featureTags' => fn($query) => $query,
-        ])
-        ->findOrFail($id);
-
-        // 「公開種別」日本語化
-        $collection->is_public_label = 
-        $collection->is_public ? '公開' : '非公開'; // trueが１、falseが0
-
-        // 「表示優先度」日本語化
-        $collection->position_label =
-        match($collection->position) {
-            0 => 'デフォルト',
-            1 => '1ページ目',
-            2 => 'topページ',
-        };
+        // コレクション&画像&技術タグのテーブルを取得
+        $collection = CollectionService::getCollectionWithRelations($id);
 
         // メイン画像
         $firstImage = $collection->collectionImages->first();
--- app/Service/PublicSite/CollectionService.php
+++ app/Service/PublicSite/CollectionService.php
@@ -1,14 +1,22 @@
 <?php 
 namespace App\Service\PublicSite;
 
+use App\Models\Collection;
+
 class CollectionService
 {
-  // ⭐️ - index ---------------------------------------------
+  // ⭐️ - show ----------------------------------------------
+  // コレクション&画像&技術タグのテーブルを取得
+  public static function getCollectionWithRelations($id) {
+      $collection = Collection::with([
+        'collectionImages' => fn($query) => $query->orderBy('position', 'asc'),
+        'technologyTags' => fn($query) => $query->orderBy('tech_type', 'asc'),
+        'featureTags' => fn($query) => $query,
+      ])
+      ->findOrFail($id);
 
-
-
-
-  // ⭐️ - 共通 - index - show --------------------------------
+      return $collection;
+  }
 
 }
 
