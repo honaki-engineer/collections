6f2155880f11a735f9d29f663ae36a23d8f1b56f
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-17 22:26:35 +0900

    views/admin/collections/index.blade.php smサイズ未満でマージン適用 修正

--- resources/views/admin/collections/index.blade.php
+++ resources/views/admin/collections/index.blade.php
@@ -31,7 +31,7 @@
                                 </option>
                             </select>
                             <button
-                                class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">検索</button>
+                                class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-1 sm:mt-0">検索</button>
                         </form>
 
                         {{-- テーブル --}}
