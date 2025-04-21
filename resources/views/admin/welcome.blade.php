94ab99ace98c5d35aab0a2630cebcd4851cc36a2
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-21 15:15:45 +0900

    /resources/views/admin/welcome.blade.php ログイン日本語化 修正

--- resources/views/admin/welcome.blade.php
+++ resources/views/admin/welcome.blade.php
@@ -36,7 +36,7 @@
                                 hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 
                                 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 
                                 transition duration-300">
-                        Login
+                        ログイン
                     </a>
                 @endauth
             </div>
