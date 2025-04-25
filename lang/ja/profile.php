5a39150064c403fddaa907b77e1f4a1f4526aaac
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-25 12:42:51 +0900

    /views/profile/partials/update-profile-information-form.blade.php 日本語化

--- lang/ja/profile.php
+++ lang/ja/profile.php
@@ -3,4 +3,12 @@
 return [
     // /views/profile/edit.blade.php
     'profile' => 'プロフィール',
+
+    // /views/profile/partials/update-profile-information-form.blade.php
+    'profile_information' => 'プロフィール情報',
+    "Update your account's profile information and email address." => 'アカウント名やメールアドレスを更新できます。',
+    'name' => '名前',
+    'email' => 'メールアドレス',
+    'save' => '更新する',
+    'saved.' => '更新しました。',
 ];
