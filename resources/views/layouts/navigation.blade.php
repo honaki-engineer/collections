7d1948d7a54c5d0cefcef80d2cd906144bc3fb93
HondaAkihito <honaki.and.1114@gmail.com>
2025-04-25 12:30:29 +0900

    /views/layouts/navigation.blade.php 日本語化

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -41,7 +41,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('navigation.profile') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -51,7 +51,7 @@
                             <x-dropdown-link :href="route('logout')"
                                     onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                                {{ __('Log Out') }}
+                                {{ __('navigation.log_out') }}
                             </x-dropdown-link>
                         </form>
                     </x-slot>
@@ -93,7 +93,7 @@
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('navigation.profile') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
@@ -103,7 +103,7 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('navigation.log_out') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
