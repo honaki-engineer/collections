de2307857eb209c7f8dd3a858a6b993aac7edb83
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-12 11:04:16 +0900

    コンポーネントfooter 作成

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,7 @@
+<!-- resources/views/components/footer.blade.php -->
+<footer class="bg-white py-4 text-center text-sm text-gray-500 shadow">
+    &copy;
+    <span class="ml-1">{{ date('Y') }}</span>
+    <span class="ml-2">{{ config('app.name') }}</span>
+    <span class="ml-2">All rights reserved</span>
+</footer>
