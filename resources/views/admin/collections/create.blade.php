9773e8f3dc5062b2be2d3080b991421dba4712d5
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-14 18:01:13 +0900

    /views/admin/collections/create.blade.php 開発背景削除

--- resources/views/admin/collections/create.blade.php
+++ resources/views/admin/collections/create.blade.php
@@ -94,16 +94,6 @@
                                                 <x-input-error :messages="$errors->get('description')" class="mt-2" />
                                             </div>
                                         </div>
-                                        {{-- 開発背景 --}}
-                                        <div class="p-2 w-full">
-                                            <div class="relative">
-                                                <label for="development_background"
-                                                    class="leading-7 text-sm text-gray-600">開発背景</label>
-                                                <textarea id="development_background" name="development_background"
-                                                    class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-y leading-6 transition-colors duration-200 ease-in-out">{{ old('development_background', session('collection.form_input.development_background')) }}</textarea>
-                                                <x-input-error :messages="$errors->get('development_background')" class="mt-2" />
-                                            </div>
-                                        </div>
                                         <div class="p-2 w-full">
                                             <div class="relative">
                                                 <label for="url_webapp" class="leading-7 text-sm text-gray-600">WebApp
