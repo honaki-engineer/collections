91f4819237048b307bd017c4e97a20d2caa60a57
HondaAkihito <honaki.and.1114@gmail.com>
2025-05-19 14:35:10 +0900

    /views/public_site/show.blade.php URLのbrを修正

--- resources/views/public_site/show.blade.php
+++ resources/views/public_site/show.blade.php
@@ -36,16 +36,20 @@
                     <img src="{{ asset('storage/collection_images/webApp.png') }}" alt="Demo"
                         class="w-5 h-5 mr-2"> Demo (Guest Login)
                 </a>
+                @if($collection->url_github || $collection->url_qiita)
+                    <br>
+                @endif
             @endif
-            <br>
             @if ($collection->url_github)
                 <a href="{{ $collection->url_github }}" target="_blank"
                     class="inline-flex items-center text-blue-600 hover:underline">
                     <img src="{{ asset('storage/collection_images/github.png') }}" alt="GitHub"
                         class="w-5 h-5 mr-2"> Github (README + Code)
                 </a>
+                @if ($collection->url_qiita)
+                    <br>
+                @endif
             @endif
-            <br>
             @if ($collection->url_qiita)
                 <a href="{{ $collection->url_qiita }}" target="_blank"
                     class="inline-flex items-center text-blue-600 hover:underline">
