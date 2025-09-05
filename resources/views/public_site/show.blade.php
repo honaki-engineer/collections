f68f592572f7402fe5aaf319ba421f2e1c2aa933
honaki <159122346+honaki-engineer@users.noreply.github.com>
2025-09-05 13:21:25 +0900

    fix: 公開showの画像余白を修正

--- resources/views/public_site/show.blade.php
+++ resources/views/public_site/show.blade.php
@@ -23,7 +23,8 @@
                 <!-- メイン画像 -->
                 <div id="mainImageContainer" class="w-full">
                     <img id="mainImage" src="{{ $mainImagePath }}" alt="メイン画像"
-                        class="w-full max-h-[600px] object-contain rounded shadow-lg">
+                        {{-- class="w-full max-h-[600px] object-contain rounded shadow-lg"> --}}
+                        class="block h-auto max-h-[min(75vh,900px)] w-auto max-w-full mx-auto object-contain rounded shadow-lg">
                 </div>
             </div>
         </div>
